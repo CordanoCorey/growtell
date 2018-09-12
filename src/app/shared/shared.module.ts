002a2fce39b83f94d705820703684918ee28970a
cordanocorey <corey@cordanosports.com>
2018-09-12 07:45:51 -0400

    layout;

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule, MatIconModule } from '@angular/material';
 import { SharedModule as IuSharedModule, WallpaperModule } from '@caiu/library';
 
 import { ContainerComponent } from './container/container.component';
@@ -6,11 +7,15 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { EmailComponent } from './email/email.component';
 import { MissionComponent } from './mission/mission.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   imports: [
     IuSharedModule,
     WallpaperModule,
+    MatIconModule,
+    MatToolbarModule,
   ],
   declarations: [
     ContainerComponent,
@@ -18,6 +23,8 @@ import { MissionComponent } from './mission/mission.component';
     FooterComponent,
     EmailComponent,
     MissionComponent,
+    NavbarComponent,
+    SidenavComponent,
   ],
   exports: [
     IuSharedModule,
